97fca2165a9c13eafc3fc9537592a3261b4d450e
Matthew Peterson <matthew251125@icloud.com>
2025-11-25 16:36:23 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Matthew Peterson on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
